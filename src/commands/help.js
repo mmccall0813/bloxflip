514a7c01c82f25ba7559cfda6f3aef8bb380492b
Mason <mmccall0813@protonmail.com>
2021-04-30 20:02:56 +0000

    🎉👻 Checkpoint ./README.md:1942716/355 ./src/commands/help.js:1942716/153

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,5 +1,10 @@
 const {MessageEmbed} = require("discord.js");
 
 module.exports = {
-  
+  name:"help",
+  execute(message){
+    var embed = new MessageEmbed();
+    var args = message.content.split(" ").slice(1);
+    
+  }
 }
