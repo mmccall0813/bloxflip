9ed657d0d2a0942e7f2ec07561379b9bac9dcb4f
Mason <mmccall0813@protonmail.com>
2021-04-30 19:27:59 +0000

    👔⚾️ Checkpoint ./src/index.js:1942716/173

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ const {token, prefix, mode} = require("./config.json");
 const fs = require("fs");
 var encryptionKey = "";
 
+var flipRequests = [];
+
 console.log("Starting...");
 console.log("Checking encrytion key...")
 
 var encryptionCheck = require("./checkEncryption.js")();
 
 encryptionKey = encryptionCheck;
+
+const client = require("discord.")
