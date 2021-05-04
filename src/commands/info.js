c3037067fee82656bd854eea046c206ec75e8cb8
Mason <mmccall0813@protonmail.com>
2021-05-04 19:21:55 +0000

    🎭💑 Checkpoint ./src/commands/info.js:1942716/436

--- src/commands/info.js
+++ src/commands/info.js
@@ -4,6 +4,7 @@ module.exports = {
   name:"info",
   execute(message){
     var embed = new MessageEmbed();
-    
+    embed.setTitle("Info")
+    .setDescription("BloxFlip is an open source dicord bot made by mmccall0813 on github. Its main feature is the coinflip, which uses ")
   }
 }
