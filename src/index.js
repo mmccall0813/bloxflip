990ce78f83fac19e5263bd76c6c14985c523062f
Mason <mmccall0813@protonmail.com>
2021-05-04 18:47:26 +0000

    🍂💏 Checkpoint ./src/index.js:1942716/363

--- src/index.js
+++ src/index.js
@@ -12,15 +12,22 @@ encryptionKey = encryptionCheck;
 config.encryptionKey = encryptionKey;
 
 const BloxClient = require("./bloxflip.js");
-const Client = new BloxClient(config);
+const client = new BloxClient(config);
 
 if(!config.prefix) {console.log("No prefix provided, defaulting to !"); config.prefix="!"}
-if(config.token) Client.login(config.token); else console.log("No token provided for discord bot.")
+if(config.token) client.login(config.token); else console.log("No token provided for discord bot.")
 
 var cmdFolder = fs.existsSync("./commands") ? fs.readdirSync("./commands/") : fs.readdirSync("./src/commands/");
 
 for(var i in cmdFolder){
   if(!cmdFolder[i].endsWith(".js")) continue;
   var command = require(`./commands/${cmdFolder[i]}`);
-  Client.commands.set(command.name, command);
+  client.commands.set(command.name, command);
 }
+client.on("ready", () => {
+  console.log("Bot is ready!");
+})
+client.on("message", (msg) => {
+  if(msg.author.bot || !msg.startsWith(config.prefix)) return;
+  
+})
